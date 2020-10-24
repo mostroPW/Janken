d50c2ee624cee335ce255e828f15de49bc59afce
mostroPW <MostroProprietorshipWorks@gmail.com>
2020-10-24 22:04:45 +0900

    保存テスト

---
+++ app/src/main/java/com/example/janken/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.janken;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    private Button myButton;
+    private Button endButton;
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("じゃんけんアプリ！！");
+
+        myButton = (Button) findViewById(R.id.myButton);
+        myButton.setOnClickListener(this);
+        endButton = (Button) findViewById(R.id.endButton);
+        endButton.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        if(v==myButton){
+                Intent intent = new Intent(this, JFrame.class);
+                startActivityForResult(intent, 0);
+            }
+
+        if(v==endButton){
+            finish();
+        }
+
+
+    }
+
+}
